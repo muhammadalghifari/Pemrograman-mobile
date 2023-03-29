41e391d596553b802e4fc7449d93074843c0b076
muhammadalghifari <114975500+muhammadalghifari@users.noreply.github.com>
2023-03-29 19:50:25 +0700

    Update PRAK203.php

--- modul2/PRAK203.php
+++ modul2/PRAK203.php
@@ -75,7 +75,7 @@ if (isset($_POST["konversi"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Soal 3 - Dwipayana Ismulya</title>
+    <title>Soal 3 - Muhammad Alghifari</title>
     <style>
         li {
             list-style: none;
